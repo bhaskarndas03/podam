fe3886ddf75f19169e708e65c23291070b8ef38c
bhaskarndas03 <31382671+bhaskarndas03@users.noreply.github.com>
2017-08-27 22:24:14 +0530

    Delete add_dependencies.sh

--- add_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo wget https://mvnrepository.com/artifact/org.eclipse.mylyn.github/org.eclipse.egit.github.core -O org.eclipse.egit.github.core-2.1.5.jar --quiet
-sudo mkdir -p ${HOME}/.m2/repository/com/group/artifact/2.1.5/
-sudo cp -rl ./org.eclipse.egit.github.core-2.1.5.jar ${HOME}/.m2/repository/com/group/artifact/1.0.0/org.eclipse.egit.github.core-2.1.5.jar
-sudo rm -f ./org.eclipse.egit.github.core-2.1.5.jar
